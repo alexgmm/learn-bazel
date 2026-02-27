9a9731642196493b42997fdf88752161c70da09d
alexgmm <alexandre.guedes.martins@gmail.com>
2026-02-27 16:44:28 -0300

    setup client generation

--- go/kube-cli/pkg/api/statectl/v1alpha1/types.go
+++ go/kube-cli/pkg/api/statectl/v1alpha1/types.go
@@ -2,7 +2,6 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // +genclient
@@ -29,24 +28,4 @@ type InstanceList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 
 	Items []Instance `json:"items"`
-}
-
-func (in *Instance) DeepCopyObject() runtime.Object {
-	if in == nil {
-		return nil
-	}
-	out := *in
-	return &out
-}
-
-func (in *InstanceList) DeepCopyObject() runtime.Object {
-	if in == nil {
-		return nil
-	}
-	out := *in
-	if in.Items != nil {
-		out.Items = make([]Instance, len(in.Items))
-		copy(out.Items, in.Items)
-	}
-	return &out
 }
